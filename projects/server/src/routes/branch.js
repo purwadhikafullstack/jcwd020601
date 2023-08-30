7c260139e78eb0a13e177804d765ff9f4c891dcf
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-08-30 14:52:58 +0700

    report responsive

--- projects/server/src/routes/branch.js
+++ projects/server/src/routes/branch.js
@@ -4,9 +4,5 @@ const branchController = require("../controllers").branchController;
 //get
 
 router.get("/", branchController.getAll);
-router.get("/:id", branchController.getById);
-router.post("/v1", branchController.insertBranch);
-router.patch("/v2/:id", branchController.editBranch);
-router.delete("/v3/:id", branchController.deleteBranch);
 
 module.exports = router;
