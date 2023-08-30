a7478081a94dc4a1128468e643e5396683b6986e
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-08-30 22:05:00 +0700

    s

--- projects/server/src/routes/cart.js
+++ projects/server/src/routes/cart.js
@@ -3,9 +3,11 @@ const getUserByToken = require("../middlewares/getuserbytoken");
 const router = express.Router();
 const cartController = require("../controllers").cartController;
 //get
+
 router.post("/id", getUserByToken, cartController.getById);
 router.post("/qty", cartController.getQty);
 router.post("/v1", getUserByToken, cartController.insertCart);
 router.patch("/v2", getUserByToken, cartController.editCart);
 router.delete("/v3/:id", getUserByToken, cartController.deleteCart);
+
 module.exports = router;
