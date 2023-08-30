b77a3b36506a1edd969ce01945b2ce8d13c61756
ThomasAlvin <thomasalvinyeo@gmail.com>
2023-08-30 13:04:56 +0700

    repost services

---
+++ projects/server/src/services/order/report.js
@@ -0,0 +1,366 @@
+const db = require("../../models");
+const { Op } = db.Sequelize;
+const Sequelize = require("sequelize");
+const opencage = require("opencage-api-client");
+const bcrypt = require("bcrypt");
+const { nanoid } = require("nanoid");
+const moment = require("moment");
+const orderReportServices = {
+  getByFilter: async (
+    { BranchName, OrderId, status, before, after, sort },
+    page1,
+    limit1
+  ) => {
+    const page = parseInt(page1) || 0;
+    const limit = parseInt(limit1) || 10;
+    const offset = limit * page;
+    const whereClause = {
+      offset: offset,
+      limit: limit,
+      where: {
+        createdAt: {
+          [Op.and]: {
+            [Op.gte]: new Date(after || "1900-01-01"),
+            [Op.lte]: new Date(before || "2100-10-10"),
+          },
+        },
+      },
+      include: {
+        model: db.Branch,
+      },
+    };
+    if (BranchName) {
+      whereClause.include.where = { name: BranchName };
+    }
+    if (OrderId) {
+      whereClause.where.id = OrderId;
+    }
+    if (status) {
+      whereClause.where.status = status;
+    }
+    if (sort?.sortedBy == "branchName") {
+      whereClause.order = [
+        [{ model: db.Branch, as: "Branch" }, "name", sort.asc],
+      ];
+    } else if (sort?.sortedBy) {
+      whereClause.order = [[sort.sortedBy, sort.asc]];
+    } else {
+      whereClause.order = [["id", sort.asc]];
+    }
+    const Order = await db.Order.findAll(whereClause);
+    const totalRows = await db.Order.count(whereClause);
+    const totalPage = Math.ceil(totalRows / limit);
+
+    return { Order, page, limit, totalRows, totalPage };
+  },
+  getTotalSalesOnLastWeek: async ({ BranchId }) => {
+    let sales = 0;
+    let quantitySold = 0;
+    const whereOrder = {
+      [Op.and]: [
+        { Status: "delivery confirm" },
+        {
+          createdAt: {
+            [db.Sequelize.Op.gte]: moment()
+              .subtract(1, "week")
+              .startOf("day")
+              .format(),
+          },
+        },
+      ],
+    };
+    const whereQuantity = {
+      where: {
+        [Op.and]: [
+          {
+            createdAt: {
+              [db.Sequelize.Op.gte]: moment()
+                .subtract(1, "week")
+                .startOf("day")
+                .format(),
+            },
+          },
+        ],
+      },
+    };
+    const whereTransaction = {
+      [Op.and]: [
+        { Status: "delivery confirm" },
+        {
+          createdAt: {
+            [db.Sequelize.Op.gte]: moment()
+              .subtract(1, "week")
+              .startOf("day")
+              .format(),
+          },
+        },
+      ],
+    };
+    if (BranchId) {
+      whereOrder.BranchId = BranchId;
+      whereQuantity.include = {
+        model: db.Order,
+        as: "Order",
+        where: { BranchId },
+      };
+      whereTransaction.BranchId = BranchId;
+    }
+    const Order = await db.Order.findAll({
+      where: whereOrder,
+    });
+    Order.map((val) => {
+      sales = val.total + sales;
+    });
+    const quantity = await db.OrderDetail.findAll(whereQuantity);
+    quantity.map((val) => {
+      quantitySold = quantitySold + val.quantity;
+    });
+    const transaction = await db.Order.findAndCountAll({
+      where: whereTransaction,
+    });
+    return {
+      Date: "From Last Week",
+      TotalSales: "Rp." + parseInt(sales).toLocaleString("id-ID"),
+      TotalSold: JSON.stringify(quantitySold),
+      TotalTransaction: transaction.count,
+    };
+  },
+  getPendingByUserId: async ({ UserId }) => {
+    const Order = await db.Order.findAll({
+      where: {
+        [Op.and]: [
+          { UserId },
+          {
+            status: {
+              [Op.or]: [
+                "waiting for payment",
+                "waiting for payment confirmation",
+                "process",
+                "sending",
+              ],
+            },
+          },
+        ],
+      },
+      include: {
+        model: db.Address,
+      },
+    });
+    return Order;
+  },
+  getHistoryByUserId: async ({ UserId }) => {
+    const Order = await db.Order.findAll({
+      where: {
+        [Op.and]: [
+          { UserId },
+          { status: { [Op.or]: ["delivery confirm", "canceled"] } },
+        ],
+      },
+      include: {
+        model: db.Address,
+      },
+    });
+    return Order;
+  },
+  getAllBranchOrder: async (page1, limit1) => {
+    const page = parseInt(page1) || 0;
+    const limit = parseInt(limit1) || 10;
+    const offset = limit * page;
+    const totalRows = await db.Order.count({});
+    const totalPage = Math.ceil(totalRows / limit);
+    const Order = await db.Order.findAll({
+      offset: offset,
+      limit: limit,
+      include: {
+        model: db.Branch,
+      },
+    });
+    return { Order, page, limit, totalRows, totalPage };
+  },
+  getSalesOnTime: async (time) => {
+    const today = new Date();
+    const Duration = new Date(today);
+    if (time == "allTime") {
+      Duration.setDate(today.getDate() - 20000);
+    } else if (time == "monthly") {
+      Duration.setDate(today.getDate() - 30);
+    } else {
+      Duration.setDate(today.getDate() - 7);
+    }
+    const Sales = await db.Order.findAll({
+      attributes: [
+        [Sequelize.fn("date", Sequelize.col("createdAt")), "date"],
+        [Sequelize.fn("sum", Sequelize.col("total")), "total_sales"],
+      ],
+      where: {
+        status: "delivery confirm",
+        createdAt: {
+          [Sequelize.Op.between]: [Duration, today],
+        },
+      },
+      group: [Sequelize.fn("date", Sequelize.col("createdAt"))],
+      raw: true,
+    });
+    let max = Math.max(...Sales.map((item) => parseInt(item.total_sales)));
+    let highest = Sales.filter((item) => parseInt(item.total_sales) === max);
+    return { sales: Sales, highest: highest[0] };
+  },
+  getSalesFromBranchIdOnTime: async (BranchId, time) => {
+    const today = new Date();
+    const Duration = new Date(today);
+    if (time == "allTime") {
+      Duration.setDate(today.getDate() - 20000);
+    } else if (time == "monthly") {
+      Duration.setDate(today.getDate() - 30);
+    } else {
+      Duration.setDate(today.getDate() - 7);
+    }
+    const Sales = await db.Order.findAll({
+      attributes: [
+        [Sequelize.fn("date", Sequelize.col("createdAt")), "date"],
+        [Sequelize.fn("sum", Sequelize.col("total")), "total_sales"],
+      ],
+      where: {
+        BranchId,
+        status: "delivery confirm",
+        createdAt: {
+          [Sequelize.Op.between]: [Duration, today],
+        },
+      },
+      group: [Sequelize.fn("date", Sequelize.col("createdAt"))],
+      raw: true,
+    });
+    let max = Math.max(...Sales.map((item) => parseInt(item.total_sales)));
+    let highest = Sales.filter((item) => parseInt(item.total_sales) === max);
+
+    return { sales: Sales, highest: highest[0] };
+  },
+  getSalesQuantityOnTime: async (time) => {
+    const today = new Date();
+    const Duration = new Date(today);
+    if (time == "allTime") {
+      Duration.setDate(today.getDate() - 20000);
+    } else if (time == "monthly") {
+      Duration.setDate(today.getDate() - 30);
+    } else {
+      Duration.setDate(today.getDate() - 7);
+    }
+    const Sales = await db.OrderDetail.findAll({
+      attributes: [
+        [Sequelize.fn("date", Sequelize.col("createdAt")), "date"],
+        [Sequelize.fn("sum", Sequelize.col("quantity")), "qty_sold"],
+      ],
+      where: {
+        createdAt: {
+          [Sequelize.Op.between]: [Duration, today],
+        },
+      },
+      group: [Sequelize.fn("date", Sequelize.col("createdAt"))],
+      raw: true,
+    });
+    let max = Math.max(...Sales.map((item) => parseInt(item.qty_sold)));
+    let highest = Sales.filter((item) => parseInt(item.qty_sold) === max);
+
+    return { sales: Sales, highest: highest[0] };
+  },
+  getSalesQuantityFromBranchIdOnTime: async (BranchId, time) => {
+    const today = new Date();
+    const Duration = new Date(today);
+    if (time == "allTime") {
+      Duration.setDate(today.getDate() - 20000);
+    } else if (time == "monthly") {
+      Duration.setDate(today.getDate() - 30);
+    } else {
+      Duration.setDate(today.getDate() - 7);
+    }
+    const Sales = await db.OrderDetail.findAll({
+      attributes: [
+        [Sequelize.fn("date", Sequelize.col("OrderDetails.createdAt")), "date"],
+        [Sequelize.fn("sum", Sequelize.col("quantity")), "qty_sold"],
+      ],
+      where: {
+        createdAt: {
+          [Sequelize.Op.between]: [Duration, today],
+        },
+      },
+      group: [Sequelize.fn("date", Sequelize.col("OrderDetails.createdAt"))],
+      include: {
+        model: db.Order,
+        as: "Order",
+        where: { BranchId },
+      },
+      raw: true,
+    });
+    let max = Math.max(...Sales.map((item) => parseInt(item.qty_sold)));
+    let highest = Sales.filter((item) => parseInt(item.qty_sold) === max);
+
+    return { sales: Sales, highest: highest[0] };
+  },
+  getTransactionOnTime: async (time) => {
+    const today = new Date();
+    const Duration = new Date(today);
+    if (time == "allTime") {
+      Duration.setDate(today.getDate() - 20000);
+    } else if (time == "monthly") {
+      Duration.setDate(today.getDate() - 30);
+    } else {
+      Duration.setDate(today.getDate() - 7);
+    }
+    const Sales = await db.Order.findAll({
+      attributes: [
+        [Sequelize.fn("date", Sequelize.col("createdAt")), "date"],
+        [Sequelize.fn("count", Sequelize.col("id")), "total_transaction"],
+      ],
+      where: {
+        status: "delivery confirm",
+        createdAt: {
+          [Sequelize.Op.between]: [Duration, today],
+        },
+      },
+      group: [Sequelize.fn("date", Sequelize.col("createdAt"))],
+      raw: true,
+    });
+    let max = Math.max(
+      ...Sales.map((item) => parseInt(item.total_transaction))
+    );
+    let highest = Sales.filter(
+      (item) => parseInt(item.total_transaction) === max
+    );
+
+    return { sales: Sales, highest: highest[0] };
+  },
+  getTransactionFromBranchIdOnTime: async (BranchId, time) => {
+    const today = new Date();
+    const Duration = new Date(today);
+    if (time == "allTime") {
+      Duration.setDate(today.getDate() - 20000);
+    } else if (time == "monthly") {
+      Duration.setDate(today.getDate() - 30);
+    } else {
+      Duration.setDate(today.getDate() - 7);
+    }
+    const Sales = await db.Order.findAll({
+      attributes: [
+        [Sequelize.fn("date", Sequelize.col("createdAt")), "date"],
+        [Sequelize.fn("count", Sequelize.col("id")), "total_transaction"],
+      ],
+      where: {
+        BranchId,
+        status: "delivery confirm",
+        createdAt: {
+          [Sequelize.Op.between]: [Duration, today],
+        },
+      },
+      group: [Sequelize.fn("date", Sequelize.col("createdAt"))],
+      raw: true,
+    });
+    let max = Math.max(
+      ...Sales.map((item) => parseInt(item.total_transaction))
+    );
+    let highest = Sales.filter(
+      (item) => parseInt(item.total_transaction) === max
+    );
+    return { sales: Sales, highest: highest[0] };
+  },
+};
+module.exports = orderReportServices;
